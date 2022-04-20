0ff497252ebf7b5b35e7ba53519a2cbceb3541bd
marilynech <nyliram.e@gmail.com>
2022-04-19 20:37:42 -0400

    made changes

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+footer.style.borderStyle = 'solid'
 
 // Select some elements...
